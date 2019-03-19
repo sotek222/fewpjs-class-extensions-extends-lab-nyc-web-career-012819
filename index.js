a894f91b6cca0cbb24b485fe92aacb61167215ce
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-19 07:30:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get getCount(){
+    let count = this.sides.length;
+    return count;
+  }
+
+  get perimeter(){
+    return this.sides.reduce((acc, curr) => acc + curr);
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid(){
+    if((this.sides[0] + this.sides[1]) > this.sides[2] && (this.sides[1] + this.sides[2]) > this.sides[0] && (this.sides[2] + this.sides[0]) > this.sides[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid(){
+    if(this.sides[0] === this.sides[2] && this.sides[1] === this.sides[3] && this.sides[0] === this.sides[1]){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get area(){
+    return this.sides[0] * this.sides[1];
+  }
+}
